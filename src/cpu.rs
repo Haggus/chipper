17973deb127852e500c886a2f4c6e19c2100e395
Mateusz 'Haggus' Mrowiec <matt.mrowiec@gmail.com>
2017-09-23 22:34:28 +0200

    Add opcode (0x8XY7)

--- src/cpu.rs
+++ src/cpu.rs
@@ -227,6 +227,18 @@ impl Chip8 {
                         self.pc += 2;
                         println!("Subtract V[{:x}] from V[{:x}]", vy, vx);
                     }
+                    0x0007 => {
+                        let (value, overflow) = self.v[vy as usize].overflowing_sub(self.v[vx as usize]);
+                        if overflow {
+                            self.v[0xF] = 0;
+                        } else {
+                            self.v[0xF] = 1;
+                        }
+                        self.v[vx as usize] = value;
+
+                        self.pc += 2;
+                        println!("Set V[{:x}] to V[{:x}] minus V[{:x}]", vx, vy, vx);
+                    }
                     _ => panic!("Unknown opcode: {:x}", self.opcode),
                 }
             }
